e2bb889053fefa65b0c2d63400564595067043f8
Havelex <lordvoldehund@gmail.com>
2024-02-28 08:14:50 +0100

    Fix md

--- src/routes/explore/sections/[section]/posts/[post]/+page.ts
+++ src/routes/explore/sections/[section]/posts/[post]/+page.ts
@@ -1,6 +1,6 @@
 import type { PageLoad } from './$types';
 
-export const loadMarkdown: PageLoad = async ({ params }) => {
+export const load: PageLoad = async ({ params }) => {
 	const item = await import(`$lib/markdown/sections/${params.section}/posts/${params.post}`);
 	const postContent = item.default;
 	const { title } = item.metadata;
