7b38eb6ae0d2492717bbc0edb0cbf92324f73ef8
Havelex <lordvoldehund@gmail.com>
2024-03-09 21:51:36 +0100

    Desperate tries to fix prisma

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,5 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()],
-	resolve: {
-		alias: {
-			'.prisma/client/index-browser': './node_modules/@prisma/client/index-browser.js'
-		}
-	}
+	plugins: [sveltekit()]
 });
