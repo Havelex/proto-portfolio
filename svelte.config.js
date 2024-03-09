4c744b912c4624d3e088785968d257895cea02f5
Havelex <lordvoldehund@gmail.com>
2024-03-09 22:16:42 +0100

    Desperate tries to fix prisma

--- svelte.config.js
+++ svelte.config.js
@@ -4,22 +4,15 @@ import { mdsvex } from 'mdsvex';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-
 	preprocess: [vitePreprocess(), mdsvex({ extensions: ['.md'], smartypants: true })],
-
 	extensions: ['.svelte', '.md'],
 
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({}),
 		alias: {
-			$markdown: './src/markdown/',
-			$static: './static/'
-		}
+			$markdown: './src/markdown/'
+		},
+		paths: {}
 	}
 };
 
