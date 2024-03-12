dffc6855f650eb39e3403daf21e8e3f9444635fd
Havelex <lordvoldehund@gmail.com>
2024-03-12 13:21:24 +0100

    Try: Fix: deployment bugs

--- src/routes/explore/+layout.svelte
+++ src/routes/explore/+layout.svelte
@@ -1,16 +1,26 @@
 <script lang="ts">
 	import SideBar from '$lib/components/SideBar/SideBar.svelte';
 	import { currentPageTitle } from '$lib/stores/stores';
+	import type { Metadata } from '$lib/types/types.js';
 	import { onDestroy, onMount } from 'svelte';
 
 	export let data;
 
 	let ready = false;
+	let sections: Map<
+		string,
+		{
+			path: string;
+			metadata: Metadata;
+		}[]
+	>;
 
 	onMount(() => (ready = true));
 	onDestroy(() => (ready = false));
 
 	$currentPageTitle = 'Explore';
+
+	$: sections = data && data.sections;
 </script>
 
 <svelte:head>
