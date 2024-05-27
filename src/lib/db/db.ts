54e11ca12df94587dad7c8f6f64be975a0bd2635
Havelex <lordvoldehund@gmail.com>
2024-05-27 14:16:36 +0200

    HotFix: Author Id null

--- src/lib/db/db.ts
+++ src/lib/db/db.ts
@@ -3,42 +3,42 @@ import { PrismaClient, type Comment, type Author } from '@prisma/client';
 const prisma = new PrismaClient();
 
 export const getCommentsFromPost = async (post: {
-  title: string;
-  section: string;
+	title: string;
+	section: string;
 }): Promise<Comment[]> => {
-  return await prisma.comment.findMany({
-    where: { postTitle: post.title, sectionTitle: post.section },
-    orderBy: [{ updatedAt: 'desc' }]
-  });
+	return await prisma.comment.findMany({
+		where: { postTitle: post.title, sectionTitle: post.section },
+		orderBy: [{ updatedAt: 'desc' }]
+	});
 };
 
 export const createComment = async (
-  postTitle: string,
-  sectionTitle: string,
-  content: string,
-  authorId: string
+	postTitle: string,
+	sectionTitle: string,
+	content: string,
+	authorId: string
 ): Promise<Comment> => {
-  return await prisma.comment.create({
-    data: {
-      postTitle: postTitle,
-      sectionTitle: sectionTitle,
-      content: content,
-      authorId: authorId
-    }
-  });
+	return await prisma.comment.create({
+		data: {
+			postTitle: postTitle,
+			sectionTitle: sectionTitle,
+			content: content,
+			authorId: authorId
+		}
+	});
 };
 
 export const updateComment = async (id: string, content: string): Promise<Comment> => {
-  return await prisma.comment.update({
-    where: { id: id },
-    data: { content: content }
-  });
+	return await prisma.comment.update({
+		where: { id: id },
+		data: { content: content }
+	});
 };
 
 export const deleteComment = async (id: string): Promise<Comment> => {
-  return await prisma.comment.delete({ where: { id: id } });
+	return await prisma.comment.delete({ where: { id: id } });
 };
 
 export const getAuthorById = async (id: string): Promise<Author | null> => {
-  return await prisma.author.findUnique({ where: { id: id } });
+	return id ? await prisma.author.findUnique({ where: { id: id } }) : null;
 };
